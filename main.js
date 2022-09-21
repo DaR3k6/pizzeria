d6ca597d4a59c6f3af04d0668635bedcca204904
unknown <kevinvilleperez@gmail.com>
2022-09-20 22:09:12 -0500

    penultima actualizacion

--- main.js
+++ main.js
@@ -1,68 +1,86 @@
-function compraJamon(){
-    let cantidadJamon = document.getElementById("cantiadJamon").value;
+function compraJamon() {
+  let cantidadJamon = document.getElementById("cantiadJamon").value;
 
-    let total = cantidadJamon * 5000;
+  cantidadJamon * 20000;
 
-    return total;    
+  return cantidadJamon;
 }
-function compraPiña(){
-    let cantidadPiña = document.getElementById("cantidadPiña").value;
-    let total = cantidadPiña * 8000;
+function compraPiña() {
+  let cantidadPiña = document.getElementById("cantidadPiña").value;
+  cantidadPiña * 22000;
 
-    return total;
-
+  return cantidadPiña;
 }
-function compraQueso(){
-    let cantidadQueso = document.getElementById("cantidadQueso").value;
-    let total = cantidadQueso * 7000;
+function compraQueso() {
+  let cantidadQueso = document.getElementById("cantidadQueso").value;
+  cantidadQueso * 25000;
 
-    return total;
-}
-
-function compraVegetariana(){
-    let cantidadVegetariana= document.getElementById("cantidadVegetariana").value;
-    let total = cantidadVegetariana * 5000;
-    return total;
+  return cantidadQueso;
 }
 
-function compraPeperoni(){
-    let cantidadPeperoni = document.getElementById("cantidadPeperoni").value; 
-    let total = cantidadPeperoni * 7000;
-    return total;
+function compraVegetariana() {
+  let cantidadVegetariana = document.getElementById(
+    "cantidadVegetariana"
+  ).value;
+  cantidadVegetariana * 25000;
+  return cantidadVegetariana;
 }
 
-
-
-function liquidar(){
-
-    let totalLiquidacion = document.getElementById("capaMostrar");  
-    let total =  compraJamon()+compraPeperoni()+compraPiña()+compraQueso()+compraQueso()
-
-    return totalLiquidacion.innerHTML = 
-    "La cantidad de pizza de Jamon es:" + compraJamon()+ 
-    '<br>'+
-    "La cantidad de pizza de Piña es:" + compraPiña()+
-    '<br>'+
-    "La cantidad de pizza de Tres Quesos es:" + compraQueso()+
-    '<br>'+
-    "La cantidad de pizza Vegetariana es:" + compraVegetariana()+
-    '<br>'+
-    "La cantidad de pizza Peperoni es:" + compraPeperoni() +
-    '<br>'+
-    "Total de la liquidacion es:" + total;
+function compraPeperoni() {
+  let cantidadPeperoni = document.getElementById("cantidadPeperoni").value;
+  cantidadPeperoni * 23000;
+  return cantidadPeperoni;
+}
+function compraDedos() {
+  let cantiadDedos = document.getElementById("cantiadadDedos").value;
+  cantiadDedos * 4000;
+  return cantiadDedos;
+}
+function compraAlitas() {
+  let cantiadadAlitas = document.getElementById("cantidadAlitas").value;
+  cantiadadAlitas * 5000;
+  return cantiadadAlitas;
+}
+function compraEnsalada() {
+  let cantidadEnsalda = document.getElementById("cantidadEnsalada").value;
+  cantidadEnsalda * 4000;
+  return cantidadEnsalda;
+}
+function compraNuggets() {
+  let cantiadadNuggets = document.getElementById("cantidadNuggets").value;
+  cantiadadNuggets * 6000;
+  return cantiadadNuggets;
+}
+function compraRolls() {
+  let cantidadRolls = document.getElementById("cantidadRolls").value;
+  cantidadRolls * 5000;
+  return cantidadRolls;
+}
+function compraHit() {
+  let cantidadHits = document.getElementById("cantiadHit").value;
+  cantidadHits * 2500;
+  return cantidadHits;
+}
+function compraHitL() {
+  let cantidadHitL = document.getElementById("cantidadHiTL").value;
+  cantidadHitL * 5000;
+  return cantidadHitL;
+}
+function compraFanta() {
+  let cantidadFanta = document.getElementById("cantidadFanta").value;
+  cantidadFanta * 3000;
+  return cantidadFanta;
+}
+function compraCocaCola() {
+  let cantidadCocaCola = document.getElementById("cantidadCocaCola").value;
+  cantidadCocaCola * 6000;
+  return cantidadCocaCola;
 }
 
+function liquidar() {
+  let totalLiquidacion = document.getElementById("capaMostrar");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  if (compraJamon() > 0) {
+    totalLiquidacion.innerHTML ="Total de pizza de Jamon es: " + compraJamon();
+  }
+}
