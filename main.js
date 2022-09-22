3ee09bf89d41abca5323283b71cbdf86e4615c35
DaR3K <kevinvilleperez@gmail.com>
2022-09-22 15:25:54 -0500

    ultima acutalizacion

--- main.js
+++ main.js
@@ -18,31 +18,37 @@ function compraJamon() {
   let cantidadJamon = document.getElementById("cantiadJamon").value;
   if(cantidadJamon <= 0){
    jamon = 0 ;
+   return jamon;
   }else{
     jamon = cantidadJamon * 20000;
+    return jamon;
   }
 
-  return jamon;
+  
 }
 function compraPiña() {
   let cantidadPiña = document.getElementById("cantidadPiña").value;
   if(cantidadPiña <= 0){
     piña = 0 ;
+    return piña;
    }else{
     piña = cantidadPiña * 22000;
+    return piña;
   }
-  return piña;
+  
 }
 function compraQueso() {
   let cantidadQueso = document.getElementById("cantidadQueso").value;
   if(cantidadQueso <= 0){
     queso= 0;
+    return queso;
   }else{
     queso = cantidadQueso * 25000;
+    return queso;
   }
 
 
-  return queso;
+  
 }
 
 function compraVegetariana() {
@@ -51,107 +57,130 @@ function compraVegetariana() {
   ).value;
   if(vegetal <= 0){
     vegetal = 0;
+    return vegetal;
   }else{
     vegetal = cantidadVegetariana * 25000;
+    return vegetal;
 
   }
-  return vegetal;
+  
 }
 
 function compraPeperoni() {
   let cantidadPeperoni = document.getElementById("cantidadPeperoni").value;
   if(cantidadPeperoni <=0){
     peperoni = 0;
+    return peperoni;
   }else{
     peperoni = cantidadPeperoni * 23000;
+    return peperoni;
   }
-  return peperoni;
+  
 }
 function compraDedos() {
   let cantiadDedos = document.getElementById("cantiadadDedos").value;
   if(cantiadDedos <= 0){
     dedos = 0;
+    return dedos;
   }else{
-  dedos  = cantiadDedos * 4000;
+    dedos  = cantiadDedos * 4000;
+    return dedos;
   }
-  return dedos;
+  
 }
 function compraAlitas() {
   let cantiadadAlitas = document.getElementById("cantidadAlitas").value;
   if(cantiadadAlitas <=0){
     alitas = 0;
+    return alitas;
   }else{
     alitas = cantiadadAlitas * 5000;
+    return alitas;
   }
-  return alitas;
+  
 }
 function compraEnsalada() {
   let cantidadEnsalda = document.getElementById("cantidadEnsalada").value;
   if(cantidadEnsalda <= 0){
     ensalada = 0;
+    return ensalada;
   }else{
     ensalada = cantidadEnsalda * 4000;
+    return ensalada;
   }
-  return ensalada;
+  
 }
 function compraNuggets() {
   let cantiadadNuggets = document.getElementById("cantidadNuggets").value;
   if(cantiadadNuggets <=0){
     nuggets = 0;
+    return nuggets;
   }else{
-  nuggets = cantiadadNuggets * 6000;
+    nuggets = cantiadadNuggets * 6000;
+    return nuggets;
   }
-  return nuggets;
+  
 }
 function compraRolls() {
   let cantidadRolls = document.getElementById("cantidadRolls").value;
   if(cantidadRolls <= 0){
     rolls = 0;
+    return rolls;
   }else{
-  rolls = cantidadRolls * 5000;
+    rolls = cantidadRolls * 5000;
+    return rolls;
   }
-  return rolls;
+  
 }
 function compraHit() {
   let cantidadHits = document.getElementById("cantiadHit").value;
   if(cantidadHits <= 0){
     hits = 0;
+    return hits;
   }else{
     hits = cantidadHits * 2500;
+    return hits;
   }
-  return hits;
+  
 }
 function compraHitL() {
   let cantidadHitL = document.getElementById("cantidadHiTL").value;
   if(cantidadHitL <= 0){
     hitl = 0;
+    return hitl;
   }else{
-  hitl = cantidadHitL * 5000;
+    hitl = cantidadHitL * 5000;
+    return hitl;
   }
-  return hitl;
+  
 }
 function compraFanta() {
   let cantidadFanta = document.getElementById("cantidadFanta").value;
   if(cantidadFanta <= 0){
     hitl = 0;
+    return fanta;
   }else{
     fanta = cantidadFanta * 3000;
+    return fanta;
   }
-  return fanta;
+  
 }
 function compraCocaCola() {
   let cantidadCocaCola = document.getElementById("cantidadCocaCola").value;
   if(cantidadCocaCola <= 0){
     coca = 0;
+    return coca;
   }else{
     coca = cantidadCocaCola * 6000;
-
+    return coca;
   }
-  return coca;
+  
 }
 
 function liquidar() {
+
   let totalLiquidacion = document.getElementById("capaMostrar");
+    prompt("Ingresa tu direccion");
 
   if (jamon > 0) {
     totalLiquidacion.innerHTML +="Total de Pizza de Jamon es: " + jamon + "<br>";   
@@ -205,7 +234,8 @@ function liquidar() {
   if(coca> 0){
     totalLiquidacion.innerHTML += "Total la Gaseosa CocaCola es: " + coca+ "<br>";
   }
-    total = jamon+piña+queso+vegetal+peperoni+dedos+alitas+ensalada+nuggets+rolls+hitl+hits+fanta+coca;
-    totalLiquidacion.innerHTML += "Total de la liquidacion:" +total+ "<br>"
+    total = jamon+piña+queso+vegetal+peperoni+dedos+alitas+ensalada+nuggets+rolls+hitl+hits+fanta+coca+2500;
+    totalLiquidacion.innerHTML += "Recuerda que tu pedido tiene un valor de: "+2500 +"<br>";
+    totalLiquidacion.innerHTML += "Total de la liquidacion:" +total+ "<br>";
 }
    
